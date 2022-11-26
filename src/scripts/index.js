1acfee1601e70862aae2565eeb1b59660dbef847
Arya Agni Pradana Firdaus <aryaagnipradana@gmail.com>
2022-11-26 20:58:24 +0700

    Initialize capstone project

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+import 'regenerator-runtime';
+import '../styles/style.css';
+import '../styles/responsive.css';
+import App from './views/app';
+
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navDrawer'),
+  content: document.querySelector('#mainContent'),
+});

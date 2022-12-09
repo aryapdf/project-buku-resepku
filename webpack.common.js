122a9501ff64ca4a7adb865e73015632b50d28d0
Faza Dinan Muhammad <109134939+fazadinanm@users.noreply.github.com>
2022-12-09 16:46:37 +0700

    update webpack.common.js
    image responsive

--- webpack.common.js
+++ webpack.common.js
@@ -35,6 +35,9 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+          globOptions: {
+            ignore: ['**/images/**'],
+          },
         },
       ],
     }),

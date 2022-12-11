40a3fc413419fd69da6662bada667ac78cb5af85
Desisuci06 <110658336+Desisuci06@users.noreply.github.com>
2022-12-11 14:32:12 +0700

    Adding content

--- src/scripts/views/pages/about-us.js
+++ src/scripts/views/pages/about-us.js
@@ -1,17 +1,61 @@
-
-
 const AboutUs = {
-  async render() {
-    return `
-          <div class="container">
-            <h2>About Us<h2>
-          </div>
-          `;
-  },
-
-  async afterRender() {
-
-  },
-};
-
-export default AboutUs;
+    async render() {
+      return `
+            <div class="container">
+              <div class="p-4">
+                <h2 class="text-center">About Us<h2>
+              </div>
+              <div class="about-container">
+              <div class="about-developers">
+                <div class="card-dev">
+                  <div class="img-dev">
+                    <img src="../images/person1.jpeg" alt="" />
+                  </div>
+                  <a href="https://www.instagram.com/faisal.ah_/?hl=id" target="_blank"><i class="fab fa-instagram"></i> faisal.ah_</a>
+                  <a href="http://linkedin.com/in/faizaladhany" target="_blank"><i class="fab fa-linkedin"></i> youngniko</a>
+                  <a href="https://github.com/youngnikoo" target="_blank"><i class="fab fa-github"></i> youngniko</a>
+                </div>
+                <div class="card-dev">
+                  <div class="img-dev">
+                    <img src="../images/person2.jpeg" alt="" />
+                  </div>
+                  <a href="https://www.instagram.com/fazadinanm/?hl=id" target="_blank"><i class="fab fa-instagram"></i> fazadinanm</a>
+                  <a href="https://www.linkedin.com/in/fazadinanm/" target="_blank"><i class="fab fa-linkedin"></i> fazadinanm</a>
+                  <a href="https://github.com/fazadinanm" target="_blank"><i class="fab fa-github"></i> fazadinanm</a>
+                </div>
+                <div class="img-wrapper">
+                  <img src="../images/icon-tab.jpg" alt="" />
+                </div>
+                <div class="card-dev">
+                  <div class="img-dev">
+                    <img src="../images/person3.jpeg" alt="" />
+                  </div>
+                  <a href="https://www.instagram.com/desinofianti_/?hl=id" target="_blank"><i class="fab fa-instagram"></i> desinofianti_</a>
+                  <a href="https://www.linkedin.com/in/desi-suci-nofianthi-a2b3a4229/" target="_blank"><i class="fab fa-linkedin"></i> desi suci</a>
+                  <a href="https://github.com/Desisuci06" target="_blank"><i class="fab fa-github"></i> desisuci06</a>
+                </div>
+                <div class="card-dev">
+                  <div class="img-dev">
+                    <img src="../images/person4.jpeg" alt="" />
+                  </div>
+                  <a href="https://www.instagram.com/_aryapdf/?hl=id" target="_blank"><i class="fab fa-instagram"></i> _aryapdf</a>
+                  <a href="https://www.linkedin.com/in/arya-agni-pradana-firdaus-174a78243/" target="_blank"><i class="fab fa-linkedin"></i> arya pradana</a>
+                  <a href="https://github.com/aryapdf" target="_blank"><i class="fab fa-github"></i> aryapdf</a>
+                </div>
+              </div>
+              <figure class="text-center mt-5">
+                <blockquote class="blockquote" style="width: 600px;">
+                  <p>Semoga dengan adanya Resepku dapat, membantu kalian dalam memasak.Tidak hanya memasak makanan yang sekedar enak, tapi juga bergizi bagi keluarga.</p>
+                </blockquote>
+                <figcaption class="blockquote-footer">
+                  Developers in <cite>Resepku</cite>
+                </figcaption>
+              </figure>
+              </div>
+            </div>
+            `;
+    },
+  };
+  
+  export default AboutUs;
+  
